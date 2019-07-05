5ca9dd02424b15d8d9d32991d10c7b521ef05404
bholcomb22 <41847159+bholcomb22@users.noreply.github.com>
2019-07-04 19:22:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require("express");
-var cors = require("cors");
-var app = express();
-var mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
 
-var MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const connectionString = "mongodb+srv://bholcomb:Saraho12@cluster0-jbwcp.mongodb.net/test?retryWrites=true&w=majority";
 
 app.use(express.static(__dirname+"/static"));
 app.use(cors());
 mongoose.connect(connectionString, {useNewUrlParser: true, dbName: "counterApp", useFindAndModify: false});
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -28,8 +28,8 @@ app.get("/api", function(req, res){
     })
 });
 app.put("/api", function(req, res){
-    var id = "5cf2b27b1c9d44000093a0fc";
-    var count = req.body;
+    let id = "5cf2b27b1c9d44000093a0fc";
+    let count = req.body;
     Counter.updateCount(id, count, {returnNewDocument: true}, function(err, counter){
         if(err){
             throw err;
